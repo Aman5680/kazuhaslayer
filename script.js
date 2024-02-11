a78c459d691b4ad07f415d5dcedc0c3eb9a8d335
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-02-11 17:09:55 +0530

    change api link to new

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const posts_countEl = document.getElementById("posts_count")
 const API_KEY = "";
 
 async function getFollowers() {
-    const url = 'https://instagram130.p.rapidapi.com/account-info?username=kazuha__slayer';
+    const url = 'https://instagram210.p.rapidapi.com/ig_profile?ig=kazuha__slayer';
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': API_KEY,
-            'X-RapidAPI-Host': 'instagram130.p.rapidapi.com'
+            'X-RapidAPI-Host': 'instagram210.p.rapidapi.com'
         }
     };
 
@@ -18,10 +18,8 @@ async function getFollowers() {
         const response = await fetch(url, options);
         const result = await response.json();
 
-        // console.log(result.edge_followed_by.count);
-
-        followers_countEl.setAttribute('data-purecounter-end', result.edge_followed_by.count);
-        posts_countEl.setAttribute('data-purecounter-end', result.edge_owner_to_timeline_media.count);
+        followers_countEl.setAttribute('data-purecounter-end', result[0].follower_count);
+        posts_countEl.setAttribute('data-purecounter-end', result[0].media_count);
 
     } catch (error) {
         console.error(error);
