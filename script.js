0eb2f83ae43f7bc9322920d7b02e8dd5c3fa606b
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2024-02-11 17:19:17 +0530

    add credentials

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const followers_countEl = document.getElementById("followers_count")
 const posts_countEl = document.getElementById("posts_count")
 
-const API_KEY = "";
+const API_KEY = "e1b4cd495fmsha012f496d4e2893p14a6e6jsna1a4b7cf2d61";
 
 async function getFollowers() {
     const url = 'https://instagram210.p.rapidapi.com/ig_profile?ig=kazuha__slayer';
